50cf77fbbd8f64c68a2a08f3faed4cfa4a245e0d
linkserendipity <462710683@qq.com>
2023-02-24 23:09:16 -0500

    hh

---
+++ FID/fid_va_ca_test.sh
@@ -0,0 +1,5 @@
+python -m pytorch_fid \
+/media/SSD/lingsen/data/CK+/results/generated_va_ca_test/128_12_500_1000_1.6 \
+/media/SSD/lingsen/data/CK+/results/VA \
+--device cuda:2
+# python -m pytorch_fid /media/SSD/lingsen/data/CK+/ckp_test/5 /media/SSD/lingsen/data/CK+/ckp_train/5 --device cuda:0
---
+++ Test_ck_eeg_emb_ca.sh
@@ -0,0 +1,8 @@
+python Test_ck_eeg_emb_ca.py --dir /media/SSD/lingsen/data/CK+/results/VA \
+--batch_size 12 \
+--timesteps 1000 \
+--epochs 600 \
+--image_size 128 \
+--save_dir /media/SSD/lingsen/code/PyTorch-DDPM/save_model_eeg/CFG_emb_ca_128_12_600_1000_ckpt.pth \
+--gpuid 4 \
+--scale 2.0
